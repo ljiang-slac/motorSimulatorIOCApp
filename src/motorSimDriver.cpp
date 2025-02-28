e9dd779f548cc372b097858d56b0751516adc040
Libo Jiang <libojiang@pc103428.slac.stanford.edu>
2025-02-28 12:33:59 -0800

    first version of the motor simulator IOC

--- src/motorSimDriver.cpp
+++ src/motorSimDriver.cpp
@@ -1,14 +1,26 @@
 #include "motorSimDriver.hpp"
-#include <iocsh.h>
-#include <epicsExport.h>
+#include <iocsh.h>    //ioc shell script
+#include <epicsExport.h>  //epics export: export functions and variables to IOC
 #include <stdio.h>
 #include <string.h>
-#include <motorRecord.h>
-#include <asynMotorController.h>
+#include <motorRecord.h>   // defines parameters for the motor record
+#include <asynMotorController.h>  //port to communicate with EPICS
 
+/////////////////////////////////////////////////////////////////////////
+/*represents a single motor axis, handling 
+ * movement(move), 
+ * polling(poll) and 
+ * stopping (stop)
+*////////////////////////////////////////////////////////////////////////
+
+
+//constructor, initialize a single motor axis (controller pointer, axis index)
+// saved the controller pointer to a member pC_
+// save the pC val to the member position_
+// setDoubleParam: add the float type numbers to the parameter list
 MotorSimAxis::MotorSimAxis(MotorSimController *pC, int axisNo)
     : asynMotorAxis(pC, axisNo), pC_(pC), position_(0.0), velocity_(0.0) {
-    asynStatus status;
+    asynStatus status;    //status to store the result of parameters 
     status = setDoubleParam(pC_->valIndex, position_);
     if (status != asynSuccess) printf("Axis %d: Failed to set VAL, status=%d\n", axisNo_, status);
     status = setDoubleParam(pC_->veloIndex, velocity_);
@@ -16,24 +28,32 @@ MotorSimAxis::MotorSimAxis(MotorSimController *pC, int axisNo)
     status = setDoubleParam(pC_->rbvIndex, position_);
     if (status != asynSuccess) printf("Axis %d: Failed to set RBV in constructor, status=%d\n", axisNo_, status);
 }
-
+//
+//
+//Function for movement relate =1 yes, 0 no;
 asynStatus MotorSimAxis::move(double position, int relative, double minVelocity, double maxVelocity, double acceleration) {
     if (relative) {
         position_ += position;
     } else {
         position_ = position;
     }
+    //update the velocity if there is any acceleration
     velocity_ = maxVelocity;
     printf("Axis %d moved to position %.2f with velocity %.2f\n", axisNo_, position_, velocity_);
+
+    //update all parameters in the list
     setDoubleParam(pC_->valIndex, position_);
     setDoubleParam(pC_->veloIndex, velocity_);
     setDoubleParam(pC_->rbvIndex, position_);
-    callParamCallbacks();
+    callParamCallbacks(); 
+
     return asynSuccess;
 }
-
+//Function "poll" to check the status periodically
 asynStatus MotorSimAxis::poll(bool *moving) {
     *moving = (velocity_ != 0.0);
+    
+    //update the readback value
     asynStatus status = setDoubleParam(pC_->rbvIndex, position_);
     if (status != asynSuccess) {
         printf("Axis %d: Failed to set RBV in poll, status=%d\n", axisNo_, status);
@@ -42,7 +62,7 @@ asynStatus MotorSimAxis::poll(bool *moving) {
     }
     return asynSuccess;
 }
-
+//Function to setup the move velocity
 asynStatus MotorSimAxis::moveVelocity(double minVelocity, double maxVelocity, double acceleration) {
     velocity_ = maxVelocity;
     printf("Axis %d moving at velocity %.2f\n", axisNo_, velocity_);
@@ -50,7 +70,7 @@ asynStatus MotorSimAxis::moveVelocity(double minVelocity, double maxVelocity, do
     callParamCallbacks();
     return asynSuccess;
 }
-
+//Function to stop the moving
 asynStatus MotorSimAxis::stop(double acceleration) {
     velocity_ = 0.0;
     printf("Axis %d stopped\n", axisNo_);
@@ -58,7 +78,7 @@ asynStatus MotorSimAxis::stop(double acceleration) {
     callParamCallbacks();
     return asynSuccess;
 }
-
+// print out the logs
 asynStatus MotorSimAxis::writeFloat64(asynUser *pasynUser, epicsFloat64 value) {
     asynStatus status = asynSuccess;
     int function = pasynUser->reason;
@@ -79,7 +99,17 @@ asynStatus MotorSimAxis::writeFloat64(asynUser *pasynUser, epicsFloat64 value) {
     callParamCallbacks();
     return status;
 }
-
+/////////////////////////////////////////////////////////////////////////
+/* Manage multple axies up to NUM_AXES
+ * nitializes parameters, and starts a poller to update states
+*/
+/////////////////////////////////////////////////////////////////////////
+/*Initialize the motor controller:MotorSimController
+* Port number, number of axis, number of motor parameters
+* interfaceMask
+* interrupt mask
+*/
+//
 MotorSimController::MotorSimController(const char *portName)
     : asynMotorController(portName, NUM_AXES, NUM_MOTOR_PARAMS,
                           0, 0,                    // interfaceMask, interruptMask
@@ -93,9 +123,10 @@ MotorSimController::MotorSimController(const char *portName)
         new MotorSimAxis(this, i);
     }
     printf("Motor simulator configured with %d axes on port %s\n", NUM_AXES, portName);
-    startPoller(0.1, 0.5, 2);
-}
 
+    startPoller(0.1, 0.5, 2);   //start the Poller    
+}
+// loop over all the axis and update the status
 asynStatus MotorSimController::poll() {
     for (int i = 0; i < NUM_AXES; i++) {
         bool moving;
@@ -104,7 +135,7 @@ asynStatus MotorSimController::poll() {
     }
     return asynSuccess;
 }
-
+//process the mother board record parameter aquisition
 asynStatus MotorSimController::drvUserCreate(asynUser *pasynUser, const char *drvInfo, const char **pptypeName, size_t *psize) {
     if (strcmp(drvInfo, "MOTOR") == 0) {
         pasynUser->reason = valIndex;
@@ -114,7 +145,7 @@ asynStatus MotorSimController::drvUserCreate(asynUser *pasynUser, const char *dr
     }
     return asynMotorController::drvUserCreate(pasynUser, drvInfo, pptypeName, psize);
 }
-
+//export the functions for the external use
 extern "C" {
     int motorSimConfigure(const char *portName) {
         new MotorSimController(portName);
